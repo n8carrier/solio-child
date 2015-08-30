fe3bb88d09d5bc789f094b12229d1741d53b229a
Nate Carrier <njcarrier@gmail.com>
2015-08-29 20:00:06 -0700

    Added footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+$g_settings = get_option('g_settings');
+?>
+
+		<footer>
+				<div class="wrapper">
+				<p>&copy; Copyright <?= date('Y'); ?> <?php bloginfo('name'); ?>&nbsp;&nbsp;&nbsp;&middot;&nbsp;&nbsp;&nbsp;Built on Wordpress with a theme adapted from <a style="footer-link" href="http://medialoot.com/item/solio-free-single-page-portfolio-wp-theme/">Solio</a></p>
+				
+			<?php wp_footer(); ?>
+			
+		</footer>
+				
+		<?php echo $g_settings['footercode']; ?>
+
+	</body>
+</html>
